05973d5b3ea0631a3f95ee5836ad089b4d5dd966
Miguel Bayón <publi.michelbayon@gmail.com>
2016-06-05 21:10:11 +0200

    Update TestFlota.java

--- actividades/TestFlota.java
+++ actividades/TestFlota.java
@@ -11,12 +11,6 @@ import org.junit.Test;
  */
 public class FlotaTest
 {
-    private Coche coche1;
-    private FurgonetaPequena furgonet1;
-    private FurgonetaGrande furgonet2;
-    private Camion camion1;
-    private Flota flota1;
-
     /**
      * Default constructor for test class FlotaTest
      */
@@ -48,12 +42,12 @@ public class FlotaTest
     @Test
     public void testFlota01()
     {
-        coche1 = new Coche(Marca.FIAT, 8, 2000, 4);
-        furgonet1 = new FurgonetaPequena(Marca.FORD, 2, 456, 6);
-        furgonet2 = new FurgonetaGrande(Marca.OPEL, 5, 20000, 1000);
-        camion1 = new Camion(Marca.CITROEN, 6, 10000, 1500);
+        Vehiculo coche1 = new Coche(Marca.FIAT, 8, 2000, 4);
+        Vehiculo furgonet1 = new FurgonetaPequena(Marca.FORD, 2, 456, 6);
+        Vehiculo furgonet2 = new FurgonetaGrande(Marca.OPEL, 5, 20000, 1000);
+        Vehiculo camion1 = new Camion(Marca.CITROEN, 6, 10000, 1500);
         
-        flota1 = new Flota();
+        Flota flota1 = new Flota();
         
         flota1.addVehiculo(coche1);
         flota1.addVehiculo(furgonet1);
@@ -73,7 +67,8 @@ public class FlotaTest
         int[] arrayEsperado3 = {3, 6, 10000, 1500};
         assertArrayEquals(arrayEsperado3, flota1.getDatosVehiculo(3));        
         
-        Coche coche2 = new Coche(Marca.FORD, 10, 300000, 4);
+        //Añadimos un coche que no cumple las medidas de seguridad
+        Vehiculo coche2 = new Coche(Marca.FORD, 10, 300000, 4);
         flota1.addVehiculo(coche2);
         
         assertEquals(false, flota1.cumplenMedidas());
@@ -83,14 +78,17 @@ public class FlotaTest
         int[] arrayEsperado4 = {4, 10, 300000, 4, 2};
         assertArrayEquals(arrayEsperado4, flota1.getDatosVehiculo(4));        
         
+        //Eliminamos de la flota el ultimo coche añadido 
         flota1.removeVehiculo(4); 
         assertEquals(true, flota1.cumplenMedidas());  
  
-        Camion camion2 = new Camion(Marca.CITROEN, 7, 206000, 4000);
+        //Añadimos un camion que no cumple las medidas de seguridad
+        Vehiculo camion2 = new Camion(Marca.CITROEN, 7, 206000, 4000);
         flota1.addVehiculo(camion2);     
         assertEquals(Marca.CITROEN, flota1.marcaMayoritaria());
         assertEquals(false, flota1.cumplenMedidas());
         
+        //Eliminamos de la flota el camion añadido
         float1.removeVehiculo(5);
         assertEquals(true, flota1.cumplenMedidas()); 
     }
