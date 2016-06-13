ea53870ec954d1f16a98e924f8ba40d6fc314764
Miguel Bayón <publi.michelbayon@gmail.com>
2016-06-13 11:02:01 +0200

    Update TestFlota.java

--- actividades/TestFlota.java
+++ actividades/TestFlota.java
@@ -62,12 +62,16 @@ public class FlotaTest
         
         assertNull(flota1.marcaMayoritaria());
         
+        // Un vehiculo con ID 0, de 8 años, con 2000 kilometros, 4 plazas y pasa revision cada 1 año
         int[] arrayEsperado0 = {0, 8, 2000, 4, 1};
         assertArrayEquals(arrayEsperado0, flota1.getDatosVehiculo(0));
+        // Un vehiculo con ID 1, de 2 años, con 456 kilometros y 6 plazas
         int[] arrayEsperado1 = {1, 2, 456, 6};
         assertArrayEquals(arrayEsperado1, flota1.getDatosVehiculo(1));
+        // Un vehiculo con ID 2, de 5 años, con 20000 kilometros y peso 1000 kilos
         int[] arrayEsperado2 = {2, 5, 20000, 1000};
         assertArrayEquals(arrayEsperado2, flota1.getDatosVehiculo(2));
+        // Un vehiculo con ID 3, de 6 años, con 10000 kilometros y 1500 kilos de peso
         int[] arrayEsperado3 = {3, 6, 10000, 1500};
         assertArrayEquals(arrayEsperado3, flota1.getDatosVehiculo(3));        
         
@@ -79,6 +83,7 @@ public class FlotaTest
         
         assertEquals(Marca.FORD, flota1.marcaMayoritaria());
         
+        // Un vehiculo con ID 4, de 10 años, con 300000 kiloemtros, de 4 plazas y pasa revision cada 2 años
         int[] arrayEsperado4 = {4, 10, 300000, 4, 2};
         assertArrayEquals(arrayEsperado4, flota1.getDatosVehiculo(4));        
         
