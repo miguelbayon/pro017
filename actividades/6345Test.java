c7a85935e8c87208901ac6f1efc08282eef56884
Miguel Bayón <miguel.bayon@iessanandres.com>
2018-05-25 09:24:32 +0200

    Create 6345Test.java

---
+++ actividades/6345Test.java
@@ -0,0 +1,107 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class CreadorDeTablerosTest.
+ */
+public class 6345Test
+{
+    /**
+     * Default constructor for test class CreadorDeTablerosTest
+     */
+    public CreadorDeTablerosTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void test01()
+    {
+        CreadorDeTableros creadorD1 = new CreadorDeTableros();
+        String resultado = "";
+        resultado += "|----------------|\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|  ##  ##  ##  ##|\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|##  ##  ##  ##  |\n";
+        resultado += "|----------------|\n";
+        assertEquals(resultado, creadorD1.devolverTablero("2 #"));
+        
+        resultado = "";
+        resultado += "|--------|\n";
+        resultado += "| @ @ @ @|\n";
+        resultado += "|@ @ @ @ |\n";
+        resultado += "| @ @ @ @|\n";
+        resultado += "|@ @ @ @ |\n";
+        resultado += "| @ @ @ @|\n";
+        resultado += "|@ @ @ @ |\n";
+        resultado += "| @ @ @ @|\n";
+        resultado += "|@ @ @ @ |\n";        
+        resultado += "|--------|\n";
+        assertEquals(resultado, creadorD1.devolverTablero("1 @"));   
+        
+        resultado = "";
+        resultado += "|------------------------|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";    
+        resultado += "|XXX   XXX   XXX   XXX   |\n";  
+        resultado += "|XXX   XXX   XXX   XXX   |\n"; 
+        resultado += "|XXX   XXX   XXX   XXX   |\n";    
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";    
+        resultado += "|XXX   XXX   XXX   XXX   |\n";  
+        resultado += "|XXX   XXX   XXX   XXX   |\n"; 
+        resultado += "|XXX   XXX   XXX   XXX   |\n"; 
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";    
+        resultado += "|XXX   XXX   XXX   XXX   |\n";  
+        resultado += "|XXX   XXX   XXX   XXX   |\n"; 
+        resultado += "|XXX   XXX   XXX   XXX   |\n"; 
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";
+        resultado += "|   XXX   XXX   XXX   XXX|\n";    
+        resultado += "|XXX   XXX   XXX   XXX   |\n";  
+        resultado += "|XXX   XXX   XXX   XXX   |\n"; 
+        resultado += "|XXX   XXX   XXX   XXX   |\n";         
+        resultado += "|------------------------|\n";
+        assertEquals(resultado, creadorD1.devolverTablero("3 X"));        
+        
+    }
+}
+
