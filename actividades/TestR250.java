5030c05132e3b021314b4c626c80ec600bef67bb
Miguel Bayón <miguel.bayon@iessanandres.com>
2018-06-12 19:02:36 +0200

    Update TestR250.java

--- actividades/TestR250.java
+++ actividades/TestR250.java
@@ -14,76 +14,77 @@ public class TestR250 {
 
     public void test02() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "" };
+        String[] inputGrid = new String[] { "" };
         assertEquals(0, objetoBase.detectarRectangulos(inputGrid));
+
     }
 
     public void test03() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { " " };
+        String[] inputGrid = new String[] { " " };
         assertEquals(0, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test04() {
-        R250 objetoBase = new R250();        
-        inputGrid = new String[] { "+-+", "| |", "+-+" };
+        R250 objetoBase = new R250();
+        String[] inputGrid = new String[] { "+-+", "| |", "+-+" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test05() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "  +-+", "  | |", "+-+-+", "| |  ", "+-+  " };
+        String[] inputGrid = new String[] { "  +-+", "  | |", "+-+-+", "| |  ", "+-+  " };
         assertEquals(2, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test06() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "  +-+", "  | |", "+-+-+", "| | |", "+-+-+" };
+        String[] inputGrid = new String[] { "  +-+", "  | |", "+-+-+", "| | |", "+-+-+" };
         assertEquals(5, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test07() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "+--+", "+--+" };
+        String[] inputGrid = new String[] { "+--+", "+--+" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test08() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "++", "||", "++" };
+        String[] inputGrid = new String[] { "++", "||", "++" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test09() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "++", "++" };
+        String[] inputGrid = new String[] { "++", "++" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test10() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "  +-+", "    |", "+-+-+", "| | -", "+-+-+" };
+        String[] inputGrid = new String[] { "  +-+", "    |", "+-+-+", "| | -", "+-+-+" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test11() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "+------+----+", "|      |    |", "+---+--+    |", "|   |       |",
+        String[] inputGrid = new String[] { "+------+----+", "|      |    |", "+---+--+    |", "|   |       |",
                 "+---+-------+" };
         assertEquals(3, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test12() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "+------+----+", "|      |    |", "+------+    |", "|   |       |",
+        String[] inputGrid = new String[] { "+------+----+", "|      |    |", "+------+    |", "|   |       |",
                 "+---+-------+" };
         assertEquals(2, objetoBase.detectarRectangulos(inputGrid));
     }
 
     public void test13() {
         R250 objetoBase = new R250();
-        inputGrid = new String[] { "+---+--+----+", "|   +--+----+", "+---+--+    |", "|   +--+----+", "+---+--+--+-+",
-                "+---+--+--+-+", "+------+  | |", "          +-+" };
+        String[] inputGrid = new String[] { "+---+--+----+", "|   +--+----+", "+---+--+    |", "|   +--+----+",
+                "+---+--+--+-+", "+---+--+--+-+", "+------+  | |", "          +-+" };
         assertEquals(60, objetoBase.detectarRectangulos(inputGrid));
     }
 }
