c8c61a41a1a5e758c0b882053224bbe81f3d65a0
Miguel Bayón <miguel.bayon@iessanandres.com>
2018-06-12 19:05:51 +0200

    Update TestR250.java

--- actividades/TestR250.java
+++ actividades/TestR250.java
@@ -12,6 +12,7 @@ public class TestR250 {
         assertEquals(0, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test02() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "" };
@@ -19,54 +20,63 @@ public class TestR250 {
 
     }
 
+    @Test
     public void test03() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { " " };
         assertEquals(0, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test04() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "+-+", "| |", "+-+" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test05() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "  +-+", "  | |", "+-+-+", "| |  ", "+-+  " };
         assertEquals(2, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test06() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "  +-+", "  | |", "+-+-+", "| | |", "+-+-+" };
         assertEquals(5, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test07() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "+--+", "+--+" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test08() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "++", "||", "++" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test09() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "++", "++" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test10() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "  +-+", "    |", "+-+-+", "| | -", "+-+-+" };
         assertEquals(1, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test11() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "+------+----+", "|      |    |", "+---+--+    |", "|   |       |",
@@ -74,6 +84,7 @@ public class TestR250 {
         assertEquals(3, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test12() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "+------+----+", "|      |    |", "+------+    |", "|   |       |",
@@ -81,6 +92,7 @@ public class TestR250 {
         assertEquals(2, objetoBase.detectarRectangulos(inputGrid));
     }
 
+    @Test
     public void test13() {
         R250 objetoBase = new R250();
         String[] inputGrid = new String[] { "+---+--+----+", "|   +--+----+", "+---+--+    |", "|   +--+----+",
